bd0da46cf5f6961c81d1ac4a1d79dc63beed42b8
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-09-17 14:24:58 +0530

    Templates added

--- src/components/CertificateEditor.jsx
+++ src/components/CertificateEditor.jsx
@@ -4,12 +4,15 @@ const CertificateEditor = ({ onUpdate }) => {
   const [data, setData] = useState({
     name: '',
     program: '',
+    description: '',
     organizer: '',
     incharge: '',
     organizerSignature: '',
     inchargeSignature: '',
     logo: '',
     id: '',
+    verificationUrl: '',
+    backgroundColor: 'red', // Default color
   });
 
   const handleChange = (e) => {
@@ -30,6 +33,12 @@ const CertificateEditor = ({ onUpdate }) => {
     }
   };
 
+  const handleColorChange = (e) => {
+    const { value } = e.target;
+    setData((prev) => ({ ...prev, backgroundColor: value }));
+    onUpdate({ ...data, backgroundColor: value });
+  };
+
   return (
     <div className="p-6 bg-white rounded shadow-lg">
       <h2 className="text-xl font-semibold mb-4">Certificate Editor</h2>
@@ -45,6 +54,13 @@ const CertificateEditor = ({ onUpdate }) => {
           type="text"
           name="program"
           placeholder="Program"
+          className="w-full p-2 border rounded"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Program Description"
           className="w-full p-2 border rounded"
           onChange={handleChange}
         />
@@ -69,6 +85,7 @@ const CertificateEditor = ({ onUpdate }) => {
           className="w-full p-2 border rounded"
           onChange={handleChange}
         />
+        <label className="block">Upload Organizer Signature:</label>
         <input
           type="file"
           name="organizerSignature"
@@ -76,6 +93,7 @@ const CertificateEditor = ({ onUpdate }) => {
           className="w-full p-2 border rounded"
           onChange={handleFileChange}
         />
+        <label className="block">Upload Incharge Signature:</label>
         <input
           type="file"
           name="inchargeSignature"
@@ -83,13 +101,32 @@ const CertificateEditor = ({ onUpdate }) => {
           className="w-full p-2 border rounded"
           onChange={handleFileChange}
         />
-      <input
+        <input
           type="text"
           name="id"
-          placeholder="CertificateId"
+          placeholder="Certificate ID"
           className="w-full p-2 border rounded"
           onChange={handleChange}
         />
+        <input
+          type="text"
+          name="verificationUrl"
+          placeholder="Verify Certificate URL"
+          className="w-full p-2 border rounded"
+          onChange={handleChange}
+        />
+        <label className="block">Background Color:</label>
+        <select
+          name="backgroundColor"
+          className="w-full p-2 border rounded"
+          value={data.backgroundColor}
+          onChange={handleColorChange}
+        >
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+        </select>
       </div>
     </div>
   );
--- src/utils/pdfmake.js
+++ src/utils/pdfmake.js
@@ -1,10 +1,28 @@
+
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 
-// Ensure vfs is set correctly
-if (pdfFonts && pdfFonts.pdfMake && pdfFonts.pdfMake.vfs) {
-  pdfMake.vfs = pdfFonts.pdfMake.vfs;
-} else {
-  console.error('pdfFonts or pdfFonts.pdfMake.vfs is undefined');
-}
+// Configure pdfMake with fonts
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
+export default pdfMake;
+
+
+// utils/pdfmake.js
+export const getBase64FromUrl = async (url) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = () => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+        reader.readAsDataURL(xhr.response);
+      };
+      xhr.onerror = reject;
+      xhr.responseType = 'blob';
+      xhr.open('GET', url, true);
+      xhr.send();
+    });
+  };
+  
