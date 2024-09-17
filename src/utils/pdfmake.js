41670efc8e4f5601e1e3cb2b7cd5f34072928056
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-09-17 11:24:11 +0530

    changes for render

--- src/utils/pdfmake.js
+++ src/utils/pdfmake.js
@@ -1,6 +1,6 @@
 // src/utils/pdfmake.js
-import * as pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 // Assign the fonts to pdfMake
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
