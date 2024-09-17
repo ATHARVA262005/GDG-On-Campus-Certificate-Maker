b994e62bd654bca703953310924b60409034070b
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-09-17 10:48:58 +0530

    changes for render

--- src/utils/pdfmake.js
+++ src/utils/pdfmake.js
@@ -1,8 +1,5 @@
-// src/utils/pdfmake.js
 import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 // Configure pdfMake with fonts
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
-export default pdfMake;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
